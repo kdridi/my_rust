abc03f4a7376489946780f79724729dc750130e7
Karim DRIDI <seth.holth@gmail.com>
2021-02-08 20:31:46 +0000

    print arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    match env::current_exe() {
+        Ok(exe_path) => println!("Path of this executable is: {}", exe_path.display()),
+        Err(e) => println!("failed to get current exe path: {}", e),
+    };
+    println!("{:?}", std::env::current_exe());
+    for argument in env::args() {
+        println!("{}", argument);
+    }
 }
